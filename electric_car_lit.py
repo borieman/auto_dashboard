2214380b09c7b881c025924b0d8123dbbda10f79
Yasinbimbam <49526065+Yasinbimbam@users.noreply.github.com>
2021-10-13 15:32:24 +0200

    Update electric_car_lit.py

--- electric_car_lit.py
+++ electric_car_lit.py
@@ -450,62 +450,62 @@ st.plotly_chart(fig5)
 
 
 #YASINNNNNNNN
-# HVAcords = [52.34590306716925,4.916398740317223]
-# lat = HVAcords[0]
-# long = HVAcords[1]
+HVAcords = [52.34590306716925,4.916398740317223]
+lat = HVAcords[0]
+long = HVAcords[1]
 
-# columnNames = ['OperatorTitle','Postcode','town','province','latitude','longitude','distance','connectTypeTitle','connectTypeDiscont','isfastcharge','levelTitle','dateLSU','dateCreated']
-# rows = []
+columnNames = ['OperatorTitle','Postcode','town','province','latitude','longitude','distance','connectTypeTitle','connectTypeDiscont','isfastcharge','levelTitle','dateLSU','dateCreated']
+rows = []
 
-# with open('opencharge_10000.json') as json_file:
-#         data = json.load(json_file)
-#         for key,document in enumerate(data):
-#             #
-#             operatorInfo = document['OperatorInfo']
-#             #als er meerdere van dit soort checks er in moeten dan kan je er een functie van maken waarbij als parameter een vervang waarde
-#             if operatorInfo :
-#                 OperatorTitle = operatorInfo['Title']
-#             else:
-#                 OperatorTitle = 'unkown'
-#             #adressinfo columns
-#             adressInfo = document['AddressInfo']
-#             #print(adressInfo)
-#             postcode = adressInfo['Postcode']
-#             town = adressInfo['Town']
-#             province = adressInfo['StateOrProvince']
-#             latitude = adressInfo['Latitude'] 
-#             longitude = adressInfo['Longitude']
-#             distance = adressInfo['Distance']
+with open('opencharge_10000.json') as json_file:
+        data = json.load(json_file)
+        for key,document in enumerate(data):
+            #
+            operatorInfo = document['OperatorInfo']
+            #als er meerdere van dit soort checks er in moeten dan kan je er een functie van maken waarbij als parameter een vervang waarde
+            if operatorInfo :
+                OperatorTitle = operatorInfo['Title']
+            else:
+                OperatorTitle = 'unkown'
+            #adressinfo columns
+            adressInfo = document['AddressInfo']
+            #print(adressInfo)
+            postcode = adressInfo['Postcode']
+            town = adressInfo['Town']
+            province = adressInfo['StateOrProvince']
+            latitude = adressInfo['Latitude'] 
+            longitude = adressInfo['Longitude']
+            distance = adressInfo['Distance']
             
-#             #Connections columns
-#             connections = document['Connections']
-#             #print('\n',connections[0],'\n',connections[1])
-#             if connections:
-#                 connectTypeTitle = connections[0]['ConnectionType']['Title']
-#                 connectTypeDiscont = connections[0]['ConnectionType']['IsDiscontinued']
+            #Connections columns
+            connections = document['Connections']
+            #print('\n',connections[0],'\n',connections[1])
+            if connections:
+                connectTypeTitle = connections[0]['ConnectionType']['Title']
+                connectTypeDiscont = connections[0]['ConnectionType']['IsDiscontinued']
               
-#             else:
-#                 connectTypeTitle = 'unkown2'
-#                 connectTypeDiscont = 'unkown3'
+            else:
+                connectTypeTitle = 'unkown2'
+                connectTypeDiscont = 'unkown3'
         
-#             if connections and connections[0]['Level']:
-#                 isfastcharge = connections[0]['Level']['IsFastChargeCapable']
-#                 levelTitle = connections[0]['Level']['Title']
-#             else:
-#                 isfastcharge = False
-#                 levelTitle = 'unkown5'
+            if connections and connections[0]['Level']:
+                isfastcharge = connections[0]['Level']['IsFastChargeCapable']
+                levelTitle = connections[0]['Level']['Title']
+            else:
+                isfastcharge = False
+                levelTitle = 'unkown5'
             
             
-#             #
-#             dateLSU = document['DateLastStatusUpdate']
-#             dateCreated = document['DateCreated']
-#             #
-#             rows.append([OperatorTitle,postcode,town,province,latitude,longitude,distance,connectTypeTitle,connectTypeDiscont,isfastcharge,levelTitle,dateLSU,dateCreated])
+            #
+            dateLSU = document['DateLastStatusUpdate']
+            dateCreated = document['DateCreated']
+            #
+            rows.append([OperatorTitle,postcode,town,province,latitude,longitude,distance,connectTypeTitle,connectTypeDiscont,isfastcharge,levelTitle,dateLSU,dateCreated])
 
 
 
-# chargingDf = pd.DataFrame(rows,columns=columnNames)
-# # chargingDf.head()
+chargingDf = pd.DataFrame(rows,columns=columnNames)
+chargingDf.head()
 
 
 # #solution: merge a new dataframe that contains the correct geograhpal information on de 'postcode' column
@@ -516,39 +516,6 @@ st.plotly_chart(fig5)
 # columns = ['OperatorTitle','Postcode','latitude','longitude','distance','connectTypeTitle','connectTypeDiscont','isfastcharge','levelTitle','dateLSU','dateCreated','Gemeente','Provincie','Latitude','Longitude']
 # DF_charging = DF_charging[columns]
 
-
-
-# import folium
-# from folium.plugins import MarkerCluster
-# #Code voor het maken van een kleurloze map van Nederland 
-# m = folium.Map(location = HVAcords, zoom_start = 7.5, tiles = 'Cartodb Positron')
-
-# #Code voor het toevoegen van drie markers op drie verschillende locaties in Nederland
-# #DF_charging2 = pd.DataFrame(DF_charging)
-# filterProvince = DF_charging['Provincie'] == 'Overijssel'
-
-# for key,charger in DF_charging[filterProvince].iterrows():
-#     #print(charger)
-#     folium.Marker(location = [charger['latitude'], charger['longitude']], popup = charger['OperatorTitle']+'\n locatie: '+str(charger['latitude'])+' '+str(charger['longitude'])).add_to(m)
-    
-    
-# folium.LayerControl().add_to(m)    
-    
-# #Laat de map zien
-# st.plotly_chart(m)
-
-
-
-
-
-
-
-
-
-# #Update de figuur om de dropdown buttons toe te voegen en laat de figuur zien
-# fig.update_layout({'updatemenus':[{'active':0, 'buttons':dropdown_buttons}]})
-
-# st.plotly_chart(fig)
 
 st.markdown(""" 
 blip bloop bloop 
